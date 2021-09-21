db543284cdcc0725da4ed242b672b2b7920352e1
Eddie Bueno <eddiebueno93@gmail.com>
2021-09-20 21:23:46 -0700

    edited config file to use environmental variables

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,4 +1,5 @@
 import os
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,7 +10,15 @@ DEBUG = True
 # Connect to the database
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1:5432")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_NAME = os.getenv("DB_NAME", "your_db")
+
+DB_PATH = "postgresql+psycopg2://{}:{}@{}/{}".format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+)
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyur'
-
-
+SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+)
