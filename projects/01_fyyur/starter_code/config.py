2aef9b06bb4c144420dbd50a4fb2b07ec92fa23b
Eddie Bueno <eddiebueno93@gmail.com>
2021-09-20 23:49:16 -0700

    changed config variables

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,15 +10,13 @@ DEBUG = True
 # Connect to the database
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1:5432")
+# IMPLEMENT DATABASE URL
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "db")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
-DB_NAME = os.getenv("DB_NAME", "your_db")
+DB_PORT = os.getenv("DB_PORT", "5432")
 
-DB_PATH = "postgresql+psycopg2://{}:{}@{}/{}".format(
-    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
-)
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(
-    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}:{}/{}".format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 )
